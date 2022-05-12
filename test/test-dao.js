e134e72607583f60fefafd487c79c8222fa26ce3
zhouyunge <zhouyunge@kuaishou.com>
2022-05-12 18:13:40 +0800

    fix: use

--- test/test-dao.js
+++ test/test-dao.js
@@ -5,13 +5,10 @@ const {
 
 
 const func = controller(async function (a, b) {
-  a = new Dao(a)
-  b = new Dao(b)
-
-  await 1
+  a = await new Dao(a)
+  b = await new Dao(b)
 
   const c = (await a.value) + b.value
-  console.log('c: ', c);
   const result = await new Dao(c)
 
   return result.value > 10 ? 'gt 10' : 'lt 10'
@@ -26,7 +23,7 @@ const func = controller(async function (a, b) {
   func.controller.variables[0].lock()
 
   setTimeout(() => {
-    func.controller.variables[0].value = 10
+    func.controller.variables[0].value = 5
     func.controller.variables[0].release()
     func.controller.variables[0].unlock()
   }, 1000)
